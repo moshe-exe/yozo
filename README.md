1b08ebb9140d6b6afe01345ea712150491a166da
Moshe Fabricio Ojeda <moshe.ojeda@rappi.com>
2023-10-10 11:48:25 -0500

    Add boto3 and SQS connection

--- README.md
+++ README.md
@@ -31,3 +31,34 @@ and not secrets with only template
 aws secretsmanager create-secret --profile personal --name yozo-my-bot-token --secret-string "VALUE" --region us-east-1
 aws secretsmanager create-secret --profile personal --name yozo-my-channel-id --secret-string "VALUE" --region us-east-1
 aws secretsmanager create-secret --profile personal --name yozo-my-group-id --secret-string "VALUE" --region us-east-1
+
+add telegram send message integration:
+
+```python
+def lambda_handler(event, context):
+    telegram_msg = event.get('message').get('text')
+    chat_id = os.environ['CHANNEL_ID']
+    telegram_token = os.environ['BOT_TOKEN']
+
+    api_url = f"https://api.telegram.org/bot{telegram_token}/"
+
+    params = {'chat_id': chat_id, 'text': telegram_msg}
+    res = requests.post(f"{api_url}sendMessage", data=params).json()
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(res),
+    }
+```
+
+deploy and test
+
+now set webhook so telegram can trigger this lambda event
+
+```bash
+curl https://api.telegram.org/bot$BOT_TOKEN/setWebhook?url=$WEBHOOK_URL
+```
+
+## Sync live
+
+sam sync --stack-name yozo-chat --watch --profile personal --region us-east-1
--- lambda-yozo-summarizer/summarizer_func/app.py
+++ lambda-yozo-summarizer/summarizer_func/app.py
@@ -1,18 +1,17 @@
 import json
 import requests
 import os
+import boto3
 
 
 def wrap_message(message):
-    DEBUG = True
     try:
-        sender = message.get('from').get('first_name')
-        text = message.get('text')
-        chat_id = message.get('chat').get('id')
-        message_id = message.get('message_id')
-        wrapped_message = f"{sender}: {text}\n"
-        if DEBUG:
-            wrapped_message += f"CHAT_ID={chat_id} MSG_ID={message_id}"
+        wrapped_message = {
+            "sender": message.get('from').get('first_name'),
+            "text": message.get('text'),
+            "chat_id": message.get('chat').get('id'),
+            "message_id": message.get('message_id')
+        }
         return wrapped_message, None
 
     except Exception as e:
@@ -31,11 +30,21 @@ def get_message(event):
     return wrap_message(event_body['message'])
 
 
-def push_to_queue(msg, queue_url):
+def push_to_queue(message, queue_url):
     if not queue_url:
         return None, "No queue url"
 
+    sqs = boto3.client('sqs')
+
     return queue_url, None
+    msg_dedup_id = f"{message['chat_id']}/{message['message_id']}"
+    response = sqs.send_message(
+            QueueUrl=queue_url,
+            MessageBody=json.dumps(message),
+            MessageGroupId='DT2_MESSAGES',
+            MessageDeduplicationId=msg_dedup_id
+        )
+    return response, None
 
 
 def lambda_handler(event, context):
@@ -53,7 +62,7 @@ def lambda_handler(event, context):
 
     api_url = f"https://api.telegram.org/bot{telegram_token}/"
 
-    params = {'chat_id': chat_id, 'text': msg_text}
+    params = {'chat_id': chat_id, 'text': str(msg_text)}
     res = requests.post(f"{api_url}sendMessage", data=params).json()
 
     return {
