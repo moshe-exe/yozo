cbb9ca7719bdd94c4d311437fe261a972a57a11d
Moshe Fabricio Ojeda <moshe.ojeda@rappi.com>
2023-10-10 13:28:14 -0500

    Change error handling

--- lambda-yozo-summarizer/summarizer_func/app.py
+++ lambda-yozo-summarizer/summarizer_func/app.py
@@ -15,43 +15,42 @@ def wrap_message(message):
             "chat_id": message.get('chat').get('id'),
             "message_id": message.get('message_id')
         }
-        return wrapped_message, None
+        return wrapped_message
 
     except Exception as e:
-        return None, str(e)
+        return str(e)
 
 
 def get_message(event):
     if 'body' not in event:
-        return None, "No message body"
+        return "No body in event"
 
     event_body = json.loads(event.get('body'))
 
     if 'message' not in event_body:
-        return None, "No message"
+        return "No message in body"
 
     return wrap_message(event_body['message'])
 
 
 def push_to_queue(message):
     if not queue_url:
-        return None, "No queue url"
+        return "No queue url found in environment"
 
-    msg_dedup_id = f"{message['chat_id']}/{message['message_id']}"
     try:
+        msg_dedup_id = f"{message['chat_id']}/{message['message_id']}"
         response = sqs.send_message(
                 QueueUrl=queue_url,
                 MessageBody=json.dumps(message),
                 MessageGroupId='DT2_MESSAGES',
                 MessageDeduplicationId=msg_dedup_id
             )
+        return response
     except Exception as e:
-        return None, str(e)
-
-    return response, None
+        return str(e)
 
 
-def poke_queue_size():
+def get_aprox_queue_size():
     try:
         response = sqs.get_queue_attributes(
             QueueUrl=queue_url,
@@ -66,20 +65,14 @@ def poke_queue_size():
 
 
 def lambda_handler(event, context):
-    processed_msg, msg_error = get_message(event)
-    queue_response, q_error = push_to_queue(processed_msg)
-    queue_size = poke_queue_size()
-
-    if msg_error:
-        processed_msg = f"MESSAGE Error: {msg_error}"
-
-    if q_error:
-        queue_response = f"QUEUE Error: {q_error}"
+    processed_msg = get_message(event)
+    queue_response = push_to_queue(processed_msg)
+    aprox_queue_size = get_aprox_queue_size()
 
     debug_dict = {
         'queue_msg': processed_msg,
         'queue_response': queue_response,
-        'poke_queue_size': queue_size
+        'aprox_queue_size': aprox_queue_size
     }
 
     chat_id = os.environ['CHANNEL_ID']
