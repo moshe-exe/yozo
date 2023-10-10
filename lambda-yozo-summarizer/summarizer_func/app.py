4db4293edbecae85324a1314b7883894ef169df2
Moshe Fabricio Ojeda <moshe.ojeda@rappi.com>
2023-10-10 10:10:05 -0500

    Add get_telegram_text function

--- lambda-yozo-summarizer/summarizer_func/app.py
+++ lambda-yozo-summarizer/summarizer_func/app.py
@@ -3,14 +3,33 @@ import requests
 import os
 
 
+def get_telegram_text(event):
+    if 'body' not in event:
+        return None, "No message body"
+
+    event_body = json.loads(event.get('body'))
+
+    if 'message' not in event_body:
+        return None, "No message"
+
+    if 'text' not in event_body['message']:
+        return None, "No message text"
+
+    return event_body['message']['text'], None
+
+
 def lambda_handler(event, context):
-    telegram_msg = event.get('message').get('text')
+    msg_text, error = get_telegram_text(event)
+
+    if error:
+        msg_text = f"Error: {error}"
+
     chat_id = os.environ['CHANNEL_ID']
     telegram_token = os.environ['BOT_TOKEN']
 
     api_url = f"https://api.telegram.org/bot{telegram_token}/"
 
-    params = {'chat_id': chat_id, 'text': telegram_msg}
+    params = {'chat_id': chat_id, 'text': msg_text}
     res = requests.post(f"{api_url}sendMessage", data=params).json()
 
     return {
