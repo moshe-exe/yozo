1b2bfd9c20a467147de9397910fff1ecdfbf6361
Moshe Fabricio Ojeda <moshe.ojeda@rappi.com>
2023-10-10 01:02:39 -0500

    Add telegram response with request

--- lambda-yozo-summarizer/summarizer_func/app.py
+++ lambda-yozo-summarizer/summarizer_func/app.py
@@ -1,9 +1,19 @@
 import json
 import requests
+import os
 
 
 def lambda_handler(event, context):
+    telegram_msg = event.get('message').get('text')
+    chat_id = os.environ['CHANNEL_ID']
+    telegram_token = os.environ['BOT_TOKEN']
+
+    api_url = f"https://api.telegram.org/bot{telegram_token}/"
+
+    params = {'chat_id': chat_id, 'text': telegram_msg}
+    res = requests.post(f"{api_url}sendMessage", data=params).json()
+
     return {
         "statusCode": 200,
-        "body": json.dumps(event.get('message').get('text')),
+        "body": json.dumps(res),
     }
